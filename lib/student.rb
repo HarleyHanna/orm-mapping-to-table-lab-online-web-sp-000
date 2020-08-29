ae9c55e2ce4dfc48d528c46b2d8bf874fe93f951
Harley Hanna <harleyhannawrit@gmail.com>
2020-08-29 14:21:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,12 +47,7 @@ class Student
   
   def self.create(name, grade)
     student = Student.new(name, grade)
-    
-  
-  
-  
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+    student.save
+    student
+  end
 end
