a18fdceb1a913305441d0796b825334bf56aa96d
Harley Hanna <harleyhannawrit@gmail.com>
2020-08-29 14:11:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,18 @@
 class Student
+  
+  attr_accessor :name, :grade, :id
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+    @@all << self
+    
+  end
+  
+  def self.all
+    @@all
+  end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
